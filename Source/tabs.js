169e5cced8717f166aa5616bbaac4a7a28d2d81f
Fabrizio S <jecnua@gmail.com>
2011-11-08 19:37:02 +0100

    I have implemented a very simple mechanism to save the last tab choice of the user on a cookie. So now it's possible to save the "state" of the tabIndex without using the hash. The code is simple and raw, but it works.

--- Source/tabs.js
+++ Source/tabs.js
@@ -30,9 +30,18 @@ MGFX.Tabs = new Class({
 	initialize: function(tabs, slides, options){
 		this.tabs = $$(tabs);
 		this.createTabs();
-		if(options.hash && window.location.hash) {
-			this.getHashIndex(options);
+		//Save
+		this.instanceTarget = tabs;
+		//Get Cookie
+		var positionCookie = getCookie(tabs);
+		//Check
+		if (positionCookie != null && positionCookie != ""){
+			options.startIndex = positionCookie;
 		}
+		//We do not use hash
+		//if(options.hash && window.location.hash) {
+		//	this.getHashIndex(options);
+		//}
 		return this.parent(slides,options);
 	},
 	
@@ -40,7 +49,9 @@ MGFX.Tabs = new Class({
 		var that = this;
 		this.tabs.forEach(function(tab,index){
 			//need index, thats why theres the forEach
-			tab.addEvent('click', function(event){ 
+			tab.addEvent('click', function(event){
+				//Set Cookie
+				setCookie(that.instanceTarget,index);
 				event.preventDefault();
 				that.showSlide(index);
 				that.stop(true);
@@ -60,3 +71,24 @@ MGFX.Tabs = new Class({
 });
 
 if(!window.Tabs) var Tabs = MGFX.Tabs;
+
+//Very simple cookie management function (source html.it)
+
+function setCookie(c_name,value,exdays) {
+	var exdate = new Date();
+	exdate.setDate(exdate.getDate() + exdays);
+	var c_value = escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
+	document.cookie=c_name + "=" + c_value;
+}
+
+function getCookie(c_name) {
+	var i,x,y,ARRcookies=document.cookie.split(";");
+	for (i=0; i<ARRcookies.length; i++) {
+	  x = ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
+	  y = ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
+	  x = x.replace(/^\s+|\s+$/g,"");
+	  if (x == c_name){
+		return unescape(y);
+	  }
+	}
+}
